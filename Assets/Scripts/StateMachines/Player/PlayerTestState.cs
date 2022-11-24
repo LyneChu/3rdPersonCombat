df530cc79cae9ba9fcba154bb6d03928faf9dc08
LyneChu <94225935+LyneChu@users.noreply.github.com>
2022-11-24 15:10:04 +0900

    State Machine setup

---
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    private float timer = 0f;
+
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter() {
+        stateMachine.InputReader.JumpEvent += OnJump;
+        Debug.Log("Enter");
+    }
+
+    public override void Tick(float deltaTime) {
+        timer += deltaTime;
+        Debug.Log($"Time : {timer:F0}s");
+    }
+
+    public override void Exit() {
+        stateMachine.InputReader.JumpEvent -= OnJump;
+        Debug.Log("Exit");
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
+    }
+}
