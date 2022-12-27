e93ecf117b6c3991dedbb7b1185010d7a377382b
LyneChu <94225935+LyneChu@users.noreply.github.com>
2022-12-27 12:17:36 +0900

    Enemy's player detection setup

--- Assets/02_Scripts/StateMachines/Enemy/EnemyBaseState.cs
+++ Assets/02_Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -9,4 +9,18 @@ public abstract class EnemyBaseState : State
     public EnemyBaseState(EnemyStateMachine stateMachine) {
         this.stateMachine = stateMachine;
     }
+
+    protected void Move(float deltaTime) {
+        Move(Vector3.zero, deltaTime);
+    }
+
+    protected void Move(Vector3 motion, float deltaTime) {
+        stateMachine.Controller.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+
+    protected bool IsInChaseRange() {
+        float playerDistanceSqr = (stateMachine.Player.transform.position - stateMachine.transform.position).sqrMagnitude;
+
+        return playerDistanceSqr <= stateMachine.PlayerChasingRange * stateMachine.PlayerChasingRange;
+    }
 }
