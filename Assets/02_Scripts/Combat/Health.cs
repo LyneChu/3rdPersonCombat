a818f2c084cb12c719d11c9b354708d52b803dfe
LyneChu <94225935+LyneChu@users.noreply.github.com>
2023-01-09 17:20:51 +0900

    Dead State setup

--- Assets/02_Scripts/Combat/Health.cs
+++ Assets/02_Scripts/Combat/Health.cs
@@ -10,19 +10,25 @@ public class Health : MonoBehaviour
     private int health;
 
     public event Action OnTakeDamage;
+    public event Action OnDie;
 
     private void Start() {
         health = maxHealth;
     }
 
     public void DealDamage(int damage) {
-        if (health == 0)
+        if (health == 0) {
             return;
+        }
 
         health = Mathf.Max(health - damage, 0);
 
         OnTakeDamage?.Invoke();
 
+        if (health == 0)
+            OnDie?.Invoke();
+
         Debug.Log($"Health : {health}");
     }
+
 }
---
+++ Assets/02_Scripts/StateMachines/Player/PlayerDeadState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeadState : PlayerBaseState
+{
+    public PlayerDeadState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter() {
+        // Todo Toggle Ragdoll
+
+        stateMachine.Weapon.gameObject.SetActive(false);
+
+    }
+
+    public override void Tick(float deltaTime) { }
+
+    public override void Exit() { }
+
+}
